5339fdffe20c66e33b41b5fd5981667b2732a6d4
moguism <mauriciorosso.aboy@gmail.com>
2025-02-13 11:19:23 +0100

    Mejorada la estética

--- app.py
+++ app.py
@@ -27,6 +27,11 @@ def image_to_base64(url):
         return base64.b64encode(response.content).decode('utf-8')
     return None
 
+# Convierte un archivo GIF a base64
+def gif_to_base64(file_path):
+    with open(file_path, "rb") as gif_file:
+        return base64.b64encode(gif_file.read()).decode('utf-8')
+
 # Función para dividir el texto en múltiples líneas si es necesario
 def wrap_text(text, max_width, font_size):
     lines = []
@@ -48,30 +53,47 @@ def get_latest_watch():
     last_watched = history[0]
     title = last_watched['title']
     thumbnail_url = last_watched['thumbnails'][0]['url']
+    artist = last_watched['artists'][0]['name']
 
     # Descargo la imagen y la convierto a base64
     base64_image = image_to_base64(thumbnail_url)
     if base64_image is None:
         return "Error al obtener la imagen", 500
+    base64_gif = gif_to_base64(constants.gif_path)
+
+    if base64_gif is None:
+        return "Error al obtener el GIF", 500
 
     # Creo el archivo
-    svg_filename = f"image_{last_watched['videoId']}.svg"
+    svg_filename = "image.svg"
     svg_path = os.path.join(image_folder, svg_filename)
 
-    # Crea el dibujo SVG con un fondo blanco
-    dwg = svgwrite.Drawing(svg_path, profile='tiny', size=(400, 300))
-    dwg.add(dwg.rect(insert=(0, 0), size=("100%", "100%"), fill="white"))
-
-    # Ajusto el texto y lo centro
+    # Cálculo del tamaño del texto
     wrapped_title = wrap_text(title, constants.max_width, constants.font_size)
     y_position = 30
     for line in wrapped_title:
-        dwg.add(dwg.text(line, insert=(200, y_position), font_size=constants.font_size, fill='black', text_anchor="middle"))
         y_position += constants.font_size + 5 # Espacio entre líneas
 
-    # Centro la imagen
-    image_x = (400 - 100) / 2 # 400 es el ancho total del SVG, 100 es el ancho de la imagen
-    dwg.add(dwg.image(f"data:image/png;base64,{base64_image}", insert=(image_x, y_position), size=(100, 100)))
+    y_position += constants.font_size + 5 # El texto del artista
+
+    svg_height = y_position + 30
+    dwg = svgwrite.Drawing(svg_path, profile='tiny', size=(constants.svg_width, svg_height))
+    dwg.add(dwg.rect(insert=(0, 0), size=(constants.svg_width, svg_height), fill="white"))
+
+    # Agrego la pesca de los textos (diría que estas medidas van bien)
+    y_position = 30
+    for line in wrapped_title:
+        dwg.add(dwg.text(line, insert=(150, y_position), font_size=constants.font_size, fill='black', font_weight="bold", text_anchor="start"))
+        y_position += constants.font_size + 5
+    dwg.add(dwg.text(artist, insert=(150, y_position), font_size=constants.font_size, fill='black', text_anchor="start"))
+
+    # Altura total del bloque de texto (título + artista)
+    total_text_height = y_position - 30  # Altura total del texto (sin incluir el margen superior)
+    center_y = (total_text_height - constants.image_height) / 2 + 30  # Centrado en el eje Y respecto al bloque de texto
+
+    # Imagen a la izquierda, centrada con respecto al texto
+    dwg.add(dwg.image(f"data:image/png;base64,{base64_image}", insert=(constants.image_x, center_y), size=(100, 100)))
+    dwg.add(dwg.image(f"data:image/gif;base64,{base64_gif}", insert=(constants.gif_x, center_y), size=(100, 100)))
 
     # Guardo el fichero y lo devuelvo
     dwg.save()
